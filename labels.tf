09709dcf00d151e75fc699b0c1b3c92ad0b1ddeb
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2023-03-25 19:46:55 +0200

    Setup  module

---
+++ labels.tf
@@ -0,0 +1,16 @@
+module "base_labels" {
+  source = "cloudposse/label/null"
+  # Cloud Posse recommends pinning every module to a specific version
+  version = "0.25.0"
+
+  namespace = var.namespace
+  stage     = var.stage
+  # name       = "bastion"
+  # attributes = ["public"]
+  # delimiter  = "-"
+
+  # tags = {
+  #   "BusinessUnit" = "XYZ",
+  #   "Snapshot"     = "true"
+  # }
+}
