c5a8cfaa64b617579822b480c35292a102b45a9d
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2023-03-25 18:38:47 +0200

    Declare and define variables

---
+++ dev.tfvars
@@ -0,0 +1,6 @@
+namespace   = "cloud_technologies_project"
+stage       = "dev"
+environment = "lpnu"
+label_order = ["stage", "namespace", "environment", "name", "attributes"]
+delimiter   = "-"
+
---
+++ vars.tf
@@ -0,0 +1,20 @@
+variable "namespace" {
+  type = string
+}
+
+variable "stage" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "label_order" {
+  type = list(string)
+}
+
+variable "delimiter" {
+  type = string
+}
+
